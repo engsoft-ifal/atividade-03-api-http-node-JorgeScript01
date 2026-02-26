9fad779c0801937dedd7a5314f92972d33a1b5b5
Jorge Matheus Bomfim <matheusblopes04@gmail.com>
2026-02-26 09:34:03 -0300

    Update server.js
    inicializa o projeto com Node.js e ESM.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
 //Código do servidor
+
+import http from "http";
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Servidor iniciado");
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
